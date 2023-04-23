2bd258c53a978c5dc6fc0004ad2a7cd427f53289
Melony Franchini <meltravelz@gmail.com>
2023-04-23 10:50:01 -0600

    feat: consume forecast endpoint - weather api

---
+++ app/services/weather_service.rb
@@ -0,0 +1,17 @@
+class WeatherService
+
+  def fetch_forecast(coordinates)
+    response = conn.get("v1/forecast.json") do |faraday|
+      faraday.params["q"] = coordinates
+      faraday.params["days"] = 5
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: "http://api.weatherapi.com/") do |faraday|
+      faraday.params["key"] = ENV["WEATHER_API_KEY"] 
+    end
+  end
+
+end
--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -1,1 +1,12 @@
 require "rails_helper"
+
+RSpec.describe WeatherService do
+  describe "instance methods" do
+    context "#fetch_all_weather_info" do
+      it "returns json object" do
+        # Los Angeles, CA coordinates = 34.05357,-118.24545
+        expect(WeatherService.new.fetch_forecast("34.05357,-118.24545")).to be_a(Hash)
+      end
+    end
+  end
+end
