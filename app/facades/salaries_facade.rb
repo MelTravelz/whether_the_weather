128c45a2658cd576bcc431c7fbfeb540b607f948
Melony Franchini <meltravelz@gmail.com>
2023-04-24 10:03:45 -0600

    found all data from service call

--- app/facades/salaries_facade.rb
+++ app/facades/salaries_facade.rb
@@ -8,9 +8,13 @@ class SalariesFacade
   def find_salary_and_forecast(destination_name)
     salaries_by_destination = @teleport_service.fetch_area_salaries(destination_name.downcase)
     
-    salaries_by_destination[:salaries].map do |salary_hash|
+    salaries = salaries_by_destination[:salaries].map do |salary_hash|
       require 'pry'; binding.pry
-
+      {
+        title: salary_hash[:job][:title],
+        min: salary_hash[:salary_percentiles][:percentile_25],
+        max: salary_hash[:salary_percentiles][:percentile_75] 
+      }
     end
   end
 
