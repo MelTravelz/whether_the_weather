d3533d4243fa6927ec0f1279130f5ad88e2e632e
Melony Franchini <meltravelz@gmail.com>
2023-04-23 23:15:37 -0600

    refactor: update user model for bcrypt

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,22 +4,23 @@ RSpec.describe User, type: :model do
   describe "validations" do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
-    it { should validate_presence_of(:password_digest) }
-
-    # when I turn bcrypt on: 
-    # it { should validate_presence_of(:password) }
-    # it { should have_secure_password }
+    it { should validate_presence_of(:password) }
+    
+    it { should have_secure_password }
+    
+    # before bcrypt: 
+    # it { should validate_presence_of(:password_digest) }
   end
 
   it "can create a user" do
-    harry = User.create(email: "harry@hogwarts.com", password_digest: "ImmaWizard!")
+    # before bcrypt:
+    # harry = User.create(email: "harry@hogwarts.com", password_digest: "ImmaWizard!")
+    # expect(harry).to have_attribute(:email)
+    # expect(harry).to have_attribute(:password_digest)
     
-    expect(harry).to have_attribute(:email)
-    expect(harry).to have_attribute(:password_digest)
-    # bcrypt:
-    # harry = User.create(email: "harry@hogwarts.com", password: "ImmaWizard!", password_confirmation: "ImmaWizard!")
-    # expect(harry).to have_attribute(:email) 
-    # expect(harry).to_not have_attribute(:password)
-    # expect(harry.password_digest).to_not eq("ImmaWizard!")
+    harry = User.create(email: "harry@hogwarts.com", password: "ImmaWizard!", password_confirmation: "ImmaWizard!")
+    expect(harry).to have_attribute(:email) 
+    expect(harry).to_not have_attribute(:password)
+    expect(harry.password_digest).to_not eq("ImmaWizard!")
   end
 end
