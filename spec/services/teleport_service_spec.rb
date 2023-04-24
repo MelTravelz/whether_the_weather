b04291a29708b89f22abf10800a290239553be20
Melony Franchini <meltravelz@gmail.com>
2023-04-24 09:15:47 -0600

    feat: add webmock fixture data for chiago salaries endpoint

--- spec/services/teleport_service_spec.rb
+++ spec/services/teleport_service_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe TeleportService do
   describe "instance methods" do
     context "#fetch_area_salaries" do
       it "can return a json object" do
+        teleport_chicago_salaries = File.read("spec/fixtures/teleport/chicago_salaries.json")
+        stub_request(:get, "https://api.teleport.org/api/urban_areas/slug:chicago/salaries/")
+        .to_return(status: 200, body: teleport_chicago_salaries, headers: {})
+
         response = TeleportService.new.fetch_area_salaries("chicago")
 
         expect(response).to be_a(Hash)
