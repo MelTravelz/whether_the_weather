4ef523654d25c0579ff80ed22f2db459c586f77c
Melony Franchini <meltravelz@gmail.com>
2023-04-25 11:15:41 -0600

    fix: remove roswell and add ny

--- spec/services/map_quest_service_spec.rb
+++ spec/services/map_quest_service_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe MapQuestService do
   describe "instance methods" do
     context "#fetch_lat_lng" do
       it "returns json object" do
-        roswell_lat_lng = File.read("spec/fixtures/map_quest/roswell_lat_lng.json")
-        stub_request(:get, "https://www.mapquestapi.com/geocoding/v1/address?key=#{ENV["MAPQUEST_API_KEY"]}&location=Roswell,%20NM")
-        .to_return(status: 200, body: roswell_lat_lng, headers: {})
+        ny_lat_lng = File.read("spec/fixtures/map_quest/ny_lat_lng.json")
+        stub_request(:get, "https://www.mapquestapi.com/geocoding/v1/address?key=#{ENV["MAPQUEST_API_KEY"]}&location=newyork,ny")
+        .to_return(status: 200, body: ny_lat_lng, headers: {})
 
-        response = MapQuestService.new.fetch_lat_lng("Roswell, NM")
+        response = MapQuestService.new.fetch_lat_lng("newyork,ny")
         keys = %i[info options results]
 
         expect(response).to be_a(Hash)
