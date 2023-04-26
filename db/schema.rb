fadcd04c82c6672bb222f5520d570585f3546215
Melony Franchini <meltravelz@gmail.com>
2023-04-25 23:14:09 -0600

    refactor: remove extra table column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_23_154653) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_26_051153) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "user_name"
     t.string "email"
     t.string "password_digest"
     t.string "api_key"
